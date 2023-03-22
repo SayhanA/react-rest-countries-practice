799980fee25b76416edf35843f09abcd7e03810d
SayhanA <sayhanahmed05@gmail.com>
2023-03-22 20:36:45 +0600

    code added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>My React App is Deployed</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
